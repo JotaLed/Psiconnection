97d4ebc8403bd92cc3055b5c0f5f074a208df545
lucasemanuel <gaitonlucas7@gmail.com>
2023-08-08 19:44:35 -0300

    Correcion de la vista home, para que no se suban los divs

--- client/src/components/searchBar/searchBar.jsx
+++ client/src/components/searchBar/searchBar.jsx
@@ -4,33 +4,33 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux'
 import { searchByName } from '../../Redux/actions'
 
-export default function SearchBar () {
-  // Obtiene la función de despacho de acciones desde Redux
-  const dispatch = useDispatch();
-  // Define el estado "searchValue" y su función "setSearchValue" para el valor de búsqueda
-  const [searchValue, setSearchValue] = useState('');
+export default function SearchBar() {
+    // Obtiene la función de despacho de acciones desde Redux
+    const dispatch = useDispatch();
+    // Define el estado "searchValue" y su función "setSearchValue" para el valor de búsqueda
+    const [searchValue, setSearchValue] = useState('');
 
-  // Función para manejar la búsqueda de recetas al hacer clic en el botón de búsqueda
-  const handleSearch = () => {
-      dispatch(searchByName(searchValue));
-  };
+    // Función para manejar la búsqueda de recetas al hacer clic en el botón de búsqueda
+    const handleSearch = () => {
+        dispatch(searchByName(searchValue));
+    };
 
-  // Función para manejar la búsqueda de recetas al presionar la tecla "Enter" en la barra de búsqueda
-  const handleKeyDown = (event) => {
-      if (event.keyCode === 13) {
-          dispatch(searchByName(searchValue));
-      }
-  };
+    // Función para manejar la búsqueda de recetas al presionar la tecla "Enter" en la barra de búsqueda
+    const handleKeyDown = (event) => {
+        if (event.keyCode === 13) {
+            dispatch(searchByName(searchValue));
+        }
+    };
 
-  // Función para manejar la búsqueda de recetas al soltar la tecla "Enter" en la barra de búsqueda
-  const handleEnter = (event) => {
-      if (event.key === 'Enter') {
-          dispatch(searchByName(searchValue))
-      }
-  };
+    // Función para manejar la búsqueda de recetas al soltar la tecla "Enter" en la barra de búsqueda
+    const handleEnter = (event) => {
+        if (event.key === 'Enter') {
+            dispatch(searchByName(searchValue))
+        }
+    };
 
     return (
-       <div className='content'>
+        <div className='content'>
             {/* Barra de búsqueda */}
             <input
                 type='search'
