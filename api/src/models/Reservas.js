f6861afbff65aa360e6fd1bd5d6831c17a8a9a64
Juan-M-Loaiza <juanmloaiz4@gmail.com>
2023-08-10 17:16:41 -0500

    nueva propiedad estado para las reservas

--- api/src/models/Reservas.js
+++ api/src/models/Reservas.js
@@ -16,6 +16,10 @@ module.exports = (sequelize) => {
         hora: {
             type: DataTypes.STRING,
             allowNull: false
+        },
+        estado: {
+            type: DataTypes.STRING,
+            allowNull: false
         }
     }, {
         timestamps: false
