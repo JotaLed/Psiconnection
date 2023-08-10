b01b8a8f0c29316ac6aa20d8d5cd2f984ad397b3
Delfina De los Santos <delfinadelossantos@gmail.com>
2023-08-10 12:11:40 -0300

    getAllApointments

--- api/src/controllers/reservaController.js
+++ api/src/controllers/reservaController.js
@@ -1,35 +1,30 @@
-const { Psicologo, Usuario, Reserva } = require('../db')
-
+const { Psicologo, Usuario, Reserva } = require("../db");
 
 const reservaCita = async ({ idPsico, idUser, fecha, hora }) => {
-    try {
-        const psicologo = await Psicologo.findByPk(idPsico);
-        const usuario = await Usuario.findByPk(idUser);
+  try {
+    const psicologo = await Psicologo.findByPk(idPsico);
+    const usuario = await Usuario.findByPk(idUser);
 
-        const nuevaReserva = {
-            fecha: fecha,
-            hora: hora
-        }
+    const nuevaReserva = {
+      fecha: fecha,
+      hora: hora,
+    };
 
-        const newReserva = await psicologo.addUsuario(usuario, {
-            through: nuevaReserva
-        });
+    const newReserva = await psicologo.addUsuario(usuario, {
+      through: nuevaReserva,
+    });
 
+    return newReserva;
+  } catch (error) {
+    console.log(error);
 
-        return newReserva;
-
-    } catch (error) {
-        console.log(error);
-        
-        throw new Error("Error al intentar reservar la cita")
-    }
+    throw new Error("Error al intentar reservar la cita");
+  }
 };
 
+const getAllAppointmentsController = async () => {
+  const appointments = await Reserva.findAll();
+  return appointments;
+};
 
-  
-
-
-
-
-
-module.exports = {reservaCita };
+module.exports = { reservaCita };
