21cc1e7cf9d72c8cdb95dd3c3591952d7a8ca936
JotaLed <jonathanledezma09@gmail.com>
2023-08-12 10:33:09 -0300

    configurando deploy en el front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,25 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Home from './views/home/home'
-import Account from './views/Account/Account'
-import SideBar from './components/sideBar/sideBar'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Home from "./views/home/home";
+import Account from "./views/Account/Account";
+import SideBar from "./components/sideBar/sideBar";
+import "./App.css";
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:3001/";
 
-//Hooks: 
-import { Routes, Route, useLocation, useNavigate } from 'react-router-dom'
+//Hooks:
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 //imports generales:
 
 //importamos views:
-import Landing from './views/landing/landing'
-import Form from './views/form/form'
-import Detail from './views/detail/detail'
-import Nosotros from './views/nosotros/nosotros'
+import Landing from "./views/landing/landing";
+import Form from "./views/form/form";
+import Detail from "./views/detail/detail";
+import Nosotros from "./views/nosotros/nosotros";
 
 //importmos components:
-
 
 //comentario
 function App() {
@@ -26,19 +27,19 @@ function App() {
 
   return (
     <div>
-      {pathname !== '/' && <SideBar/>}
+      {pathname !== "/" && <SideBar />}
       <Routes>
-        <Route path='/form' element={<Form />} />
+        <Route path="/form" element={<Form />} />
 
-        <Route path='/' element={<Landing />} />
-        <Route path='/home' element={<Home />} /> 
-        <Route path='/nosotros' element={<Nosotros/>}></Route>
-        <Route path="/detail/:detailID" element={<Detail/>}></Route>
-        <Route path='/account/:id' element={<Account/>}/>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/nosotros" element={<Nosotros />}></Route>
+        <Route path="/detail/:detailID" element={<Detail />}></Route>
+        <Route path="/account/:id" element={<Account />} />
       </Routes>
     </div>
-  )
+  );
 }
-export default App
+export default App;
 
 //prueba
--- client/src/Redux/actions.js
+++ client/src/Redux/actions.js
@@ -1,69 +1,68 @@
 export const SET_FILTER = "SET_FILTER";
 export const SET_ORDERS = "SET_ORDERS";
 export const GET_PSICOLOGOS = "GET_PSICOLOGOS";
-export const LOAD_DETAIL = "LOAD_DETAIL"
-export const SEARCH_APELLIDO = "SEARCH_APELLIDO"
+export const LOAD_DETAIL = "LOAD_DETAIL";
+export const SEARCH_APELLIDO = "SEARCH_APELLIDO";
 export const UPDATE_PSIC = "UPDATE_PSIC";
-import axios from "axios"
+import axios from "axios";
 
 export const setFilter = (filters) => {
-    //comentario
-    return { type: SET_FILTER, payload: filters }
-}
+  //comentario
+  return { type: SET_FILTER, payload: filters };
+};
 
 export const setOrders = (order) => {
-    return { type: SET_ORDERS, payload: order }
-}
+  return { type: SET_ORDERS, payload: order };
+};
 //Action de get psicologos
 export const getPsicologos = () => {
-    return async function (dispatch) {
-            const apiData = await axios.get("http://localhost:3001/psiconection/");
-            const psicologos = apiData.data
-           dispatch({ type: GET_PSICOLOGOS, payload: psicologos })
-        
-
-    }
-}//Action de get by ID
+  return async function (dispatch) {
+    const apiData = await axios.get("/psiconection/");
+    const psicologos = apiData.data;
+    dispatch({ type: GET_PSICOLOGOS, payload: psicologos });
+  };
+}; //Action de get by ID
 export const loadDetail = (id) => {
-    return (async function (dispatch) {
-        try {
-            const { data } = await axios.get(`http://localhost:3001/psiconection/${id}`);
-            const detail = data
-            return (dispatch({
-                type: LOAD_DETAIL, payload: detail
-            }))
-        } catch (error) {
-            console.log(error.message);
-
-        }
-    })
-}
+  return async function (dispatch) {
+    try {
+      const { data } = await axios.get(`/psiconection/${id}`);
+      const detail = data;
+      return dispatch({
+        type: LOAD_DETAIL,
+        payload: detail,
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+};
 //action para buscar por name
 export const searchByName = (apellido) => {
-    return async function (dispatch) {
-        try {
-            const response = await axios.get(`http://localhost:3001/psiconection/?apellido=${apellido}`)
-            if (response.data.length === 0) {
-                alert('Name not found!');
-            } else {
-                return dispatch({
-                    type: 'SEARCH_APELLIDO',
-                    payload: response.data
-                })
-            }
-        } catch (error) {
-            window.alert('Name not found!');
-        }
+  return async function (dispatch) {
+    try {
+      const response = await axios.get(`/psiconection/?apellido=${apellido}`);
+      if (response.data.length === 0) {
+        alert("Name not found!");
+      } else {
+        return dispatch({
+          type: "SEARCH_APELLIDO",
+          payload: response.data,
+        });
+      }
+    } catch (error) {
+      window.alert("Name not found!");
     }
-}
+  };
+};
 
 export const updatePsic = (dataToUpdate) => {
-    axios.put("http://localhost:3001/psiconection/update", dataToUpdate)
-        .then(response => {
-            window.alert("Datos actualizados!")
-        })
-        .catch(error => {
-            console.log("error")
-            // Manejar errores en caso de que la solicitud falle
-        });
-}
+  axios
+    .put("/psiconection/update", dataToUpdate)
+    .then((response) => {
+      window.alert("Datos actualizados!");
+    })
+    .catch((error) => {
+      console.log("error");
+      // Manejar errores en caso de que la solicitud falle
+    });
+};
