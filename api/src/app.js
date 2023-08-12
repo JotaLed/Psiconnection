fa8af296e8dd369368c9f329cf948e325faf77f0
JotaLed <jonathanledezma09@gmail.com>
2023-08-12 11:54:22 -0300

    acomodando link deploy desde el front

--- api/src/app.js
+++ api/src/app.js
@@ -13,7 +13,10 @@ server.use(morgan("dev"));
 server.use((req, res, next) => {
   // res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
   // res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header("Access-Control-Allow-Origin", "https://psiconnection.vercel.app");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://psiconnection-drtzip4aq-jotaled.vercel.app/"
+  );
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
