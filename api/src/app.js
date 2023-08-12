3a2fce24d23eaeb871eef17308ffbbe7928e6720
JotaLed <jonathanledezma09@gmail.com>
2023-08-12 11:58:12 -0300

    probandooo

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,7 @@ server.use((req, res, next) => {
   // res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.header(
     "Access-Control-Allow-Origin",
-    "https://psiconnection-drtzip4aq-jotaled.vercel.app/"
+    "https://psiconnection-drtzip4aq-jotaled.vercel.app"
   );
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
