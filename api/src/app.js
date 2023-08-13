121ce6eb910c45193f8d1469748cf2e4455b601b
JotaLed <jonathanledezma09@gmail.com>
2023-08-13 11:02:23 -0300

    cors 2

--- api/src/app.js
+++ api/src/app.js
@@ -24,9 +24,9 @@ server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
   const allowedOrigins = [
-    //"https://psiconnection-drtzip4aq-jotaled.vercel.app",
-    "https://psiconnection-6obk46mug-jotaled.vercel.app",
-    "https://psiconnection.vercel.app",
+    // "https://psiconnection-drtzip4aq-jotaled.vercel.app",
+    "https://psiconnection-1lfz0xsz8-jotaled.vercel.app/",
+    // "https://psiconnection.vercel.app",
     "http://localhost:3000/",
     "http://localhost:5173",
     // Agrega más orígenes si es necesario
