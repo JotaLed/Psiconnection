1c9dc03431bf6f4487fed69df51bf8bdf9cec15d
JotaLed <jonathanledezma09@gmail.com>
2023-08-13 10:36:12 -0300

    suplantadas las a por Link

--- client/src/components/sideBar/sideBar.jsx
+++ client/src/components/sideBar/sideBar.jsx
@@ -1,29 +1,25 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import "./sidebar.css";
-//importamos imagen del logo
 import logo from "../../Images/Screenshot_18.jpg";
 
 export default function Sidebar() {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        {/* <a href="/home" className="navbar-logo">Psiconnection</a> */}
-        <Link to="/home">
-          <div className="navbar-logo_conteiner">
-            <img className="navbar-logo" src={logo} alt="" />
-          </div>
+        <Link to="/home" className="navbar-logo_conteiner">
+          <img className="navbar-logo" src={logo} alt="" />
         </Link>
         <ul className="nav-menu">
           <li className="nav-item">
-            <a href="/home">Home</a>
+            <Link to="/home">Home</Link>
           </li>
-          {/* <li className="nav-item"><a href="/detail">Perfil</a></li> */}
+          {/* <li className="nav-item"><Link to="/detail">Perfil</Link></li> */}
           <li className="nav-item">
-            <a href="/nosotros">About us</a>
+            <Link to="/nosotros">About us</Link>
           </li>
           <li className="nav-item">
-            <a href="/form">Sign up</a>
+            <Link to="/form">Sign up</Link>
           </li>
         </ul>
       </div>
