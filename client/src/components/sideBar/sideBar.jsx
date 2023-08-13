a54ebfa72762acfa0f487580638a8ae8ab56c2a4
JotaLed <jonathanledezma09@gmail.com>
2023-08-13 10:05:33 -0300

    acomodada la importación de estilos en sideBar

--- client/src/components/sideBar/sideBar.jsx
+++ client/src/components/sideBar/sideBar.jsx
@@ -1,28 +1,32 @@
-import React, { useState } from 'react';
-import {Link} from 'react-router-dom'
-import './Sidebar.css';
-//importamos imagen del logo 
-import logo from "../../Images/Screenshot_18.jpg"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./sidebar.css";
+//importamos imagen del logo
+import logo from "../../Images/Screenshot_18.jpg";
 
 export default function Sidebar() {
-
   return (
-<nav className="navbar">
+    <nav className="navbar">
       <div className="navbar-container">
         {/* <a href="/home" className="navbar-logo">Psiconnection</a> */}
         <Link to="/home">
-          <div className='navbar-logo_conteiner'> 
-            <img className='navbar-logo' src={logo} alt="" />
+          <div className="navbar-logo_conteiner">
+            <img className="navbar-logo" src={logo} alt="" />
           </div>
         </Link>
         <ul className="nav-menu">
-          <li className="nav-item"><a href="/home">Home</a></li>
+          <li className="nav-item">
+            <a href="/home">Home</a>
+          </li>
           {/* <li className="nav-item"><a href="/detail">Perfil</a></li> */}
-          <li className="nav-item"><a href="/nosotros">About us</a></li>
-          <li className="nav-item"><a href="/form">Sign up</a></li>
-        </ul> 
+          <li className="nav-item">
+            <a href="/nosotros">About us</a>
+          </li>
+          <li className="nav-item">
+            <a href="/form">Sign up</a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
 }
-
