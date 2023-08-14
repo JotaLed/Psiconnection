68beccd0e81e94e6588487474e29b2e1b6d1c745
Jonathan Ledezma <105135057+JotaLed@users.noreply.github.com>
2023-08-14 12:29:43 -0300

    Delete api directory

--- api/src/routes/specialtiesRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const getSpecialties = require("../controllers/specialtiesController");
-
-const specialtiesRoutes = Router();
-
-specialtiesRoutes.get("/", getSpecialties);
-
-module.exports = specialtiesRoutes;
