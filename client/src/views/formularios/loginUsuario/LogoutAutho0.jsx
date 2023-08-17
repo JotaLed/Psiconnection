38658fd5668022509f361a9d1a9d908d19fdba94
JotaLed <jonathanledezma09@gmail.com>
2023-08-17 16:42:44 -0300

    acomodando logout de terceros

--- client/src/views/formularios/loginUsuario/LogoutAutho0.jsx
+++ client/src/views/formularios/loginUsuario/LogoutAutho0.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 const LogoutButtonAuth0 = () => {
   const { logout } = useAuth0();
+  const navigate = useNavigate();
 
-  return <button onClick={() => logout()}>logout</button>;
+  const handleLogout = () => {
+    logout({ returnTo: `${window.location.origin}/home` }); // Return to the /home page
+  };
+
+  return <button onClick={handleLogout}>logout</button>;
 };
 
 export default LogoutButtonAuth0;
