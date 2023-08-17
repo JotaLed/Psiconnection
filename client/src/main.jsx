436625367578e72182e26f906373eeb06fe7f4b8
JotaLed <jonathanledezma09@gmail.com>
2023-08-17 13:47:48 -0300

    casi listo

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,13 +7,7 @@ import { Provider } from "react-redux";
 import { Auth0Provider } from "@auth0/auth0-react";
 import store from "./Redux/store";
 
-const onRedirectCallback = (appState) => {
-  window.location.href = "http://localhost:5173/registroUsuario/google";
-};
-
-const onLogout = () => {
-  window.location.href = "http://localhost:5173/home";
-};
+const rootUri = window.location.origin;
 
 const root = createRoot(document.getElementById("root"));
 
@@ -22,9 +16,11 @@ root.render(
     <Auth0Provider
       domain={"dev-sdzlhz4u3748bi1n.us.auth0.com"}
       clientId={"pqcJSZZkLUgo8FLZD2vpRH76UsQVNcb5"}
-      onRedirectCallback={onRedirectCallback}
-      onLogout={onLogout}
-      redirectUri={window.location.origin}
+      redirectUri={
+        rootUri.includes("localhost")
+          ? "http://localhost:5173/registroUsuario/google"
+          : "https://psiconnectiondev.vercel.app/registroUsuario/google"
+      }
     >
       <BrowserRouter>
         <App />
@@ -32,3 +28,51 @@ root.render(
     </Auth0Provider>
   </Provider>
 );
+
+// import React from "react";
+// import { createRoot } from "react-dom";
+// import { BrowserRouter } from "react-router-dom";
+// import App from "./App.jsx";
+// import "./index.css";
+// import { Provider } from "react-redux";
+// import { Auth0Provider } from "@auth0/auth0-react";
+// import store from "./Redux/store";
+// import axios from "axios";
+
+// const onRedirectCallback = async ({ user }) => {
+//   if (user) {
+//     try {
+//       const response = await axios.get("psiconection/get/users");
+//       const usersData = response.data || [];
+//       const userEmail = user.email;
+
+//       if (userEmailIsRegistered(usersData, userEmail)) {
+//         window.location.href = "http://localhost:5173/home";
+//       } else {
+//         window.location.href = "http://localhost:5173/registroUsuario/google";
+//       }
+//     } catch (error) {
+//       console.error("Error fetching user data:", error);
+//     }
+//   }
+// };
+
+// const userEmailIsRegistered = (usersData, userEmail) => {
+//   return usersData.some((user) => user.email === userEmail);
+// };
+
+// const root = createRoot(document.getElementById("root"));
+
+// root.render(
+//   <Provider store={store}>
+//     <Auth0Provider
+//       domain={"dev-sdzlhz4u3748bi1n.us.auth0.com"}
+//       clientId={"pqcJSZZkLUgo8FLZD2vpRH76UsQVNcb5"}
+//       onRedirectCallback={onRedirectCallback}
+//     >
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </Auth0Provider>
+//   </Provider>
+// );
--- client/src/views/formularios/loginUsuario/LoginAuth0.jsx
+++ client/src/views/formularios/loginUsuario/LoginAuth0.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 const LoginButtonAuth0 = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return (
-    <button
-      onClick={() => {
-        loginWithRedirect();
-      }}
-    >
-      Login con Google
-    </button>
-  );
+  const handleLogin = async () => {
+    await loginWithRedirect();
+  };
+
+  return <button onClick={handleLogin}>Login con Google</button>;
 };
 
 export default LoginButtonAuth0;
