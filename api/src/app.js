db76f1e5535b168ab389fd13e96cbe6709fccfdc
JotaLed <jonathanledezma09@gmail.com>
2023-08-22 14:58:50 -0300

    werdfgd

--- api/src/app.js
+++ api/src/app.js
@@ -19,7 +19,7 @@ const config = {
 
 const server = express();
 
-// server.use(cors());
+server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
